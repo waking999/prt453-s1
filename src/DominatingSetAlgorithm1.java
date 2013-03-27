d7a8a605ee88b846c3711f8b30963e937a26bcc1
Kevin <waking.wang@gmail.com>
2013-03-27 16:59:57 +0930

    initial edition for creating cm environment

---
+++ src/DominatingSetAlgorithm1.java
@@ -0,0 +1,10 @@
+/**
+ * This class works for the basic algorithm to solve dominating set problem
+ * @author : Kai
+ *
+ */
+public class DominatingSetAlgorithm1 {
+	public void generateDominatingSet(){
+		
+	}
+}
---
+++ src/DominatingSetAlgorithm1Test.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class DominatingSetAlgorithm1Test extends TestCase {
+	@Test
+	public void testGenerateDominatingSet(){
+		assertTrue(true);
+	}
+}
